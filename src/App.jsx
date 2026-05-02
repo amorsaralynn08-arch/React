4b01719ad1caea0e5f22b61b6fdf39b47dcb64c7
amor <amorsaralynn@gmail.com>
2026-05-02 21:18:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Dashboard from "./Components/Dashboard"
+import Home from "./Components/Home"
+import Products from "./Components/Products"
+import Register from "./Components/Register"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Waaaaah</h1>
+    <Home/>
+    <Register/>
+    <Dashboard/>
+    <Products/>
+    </>
+  )
+}
+
+export default App
